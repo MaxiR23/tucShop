3793788ee3e1f263c418151207642add3d23dcb4
Maxi Rebolo <maximilianorebolo@gmail.com>
2019-12-23 19:10:55 -0300

    Pruebas y modificaciones  2

--- app/src/main/java/com/example/tucshop/ActivityProductos/ProductosActivity.java
+++ app/src/main/java/com/example/tucshop/ActivityProductos/ProductosActivity.java
@@ -3,8 +3,6 @@ package com.example.tucshop.ActivityProductos;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,62 +16,177 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class ProductosActivity extends AppCompatActivity {
 
-    private TextView nombreProducto, precioProducto, gpu, tvgName;
+    private TextView nombreProducto, precioProducto, tvAlto, tvAncho, tvBateria, tvcamFront, tvcamPrincipal, tvCantParlantes, tvCapacBateria,
+            tvGpu, tvHMDI, tvLinea, tvMarca,tvMarcaProcesador, tvMemRam, tvMemInterna, tvModelProcesador, tvNuleos, tvPeso, tvRed, tvResolucion, tvSo, tvTamPantalla, tvTipoPantalla, tvUsb, tvversionSO, tvWifi;
     private ImageView imagenProducto;
     private DatabaseReference reference;
-    private LinearLayout linearGpu;
-    String nameProduct, priceProduct, imageProd;
-    Context context;
+    private LinearLayout linearBateria, linearcamaraPrincipal, linearcamaraFront, linearCantidadParlantes, linearCapacBateria, linearGPU, linearHMDI, linearMemRam, linearMemInt, linearMarcaProcesador,
+            linearModelProcesador, linearNucleos, linearRed, linearSO, linearUSB, linearVSO, linearWifi;
+    String nameProduct, priceProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
 
+        //TV Principales
         nombreProducto = findViewById(R.id.nombreProducto);
         precioProducto = findViewById(R.id.precioProducto);
         imagenProducto = findViewById(R.id.imageProducto);
-
-        linearGpu = findViewById(R.id.linearGPU);
-        gpu = findViewById(R.id.tvGPU);
-        tvgName = findViewById(R.id.tvGpuName);
 
         nameProduct = getIntent().getStringExtra("nombreProducto");
         priceProduct = getIntent().getStringExtra("montoProducto");
 
         nombreProducto.setText(nameProduct);
         precioProducto.setText(priceProduct);
+        //
 
-        reference = FirebaseDatabase.getInstance().getReference("Productos").child("1");
+        //TV de las caracteristicas
+
+        tvAlto = findViewById(R.id.tvAlto);
+        tvAncho = findViewById(R.id.tvAncho);
+        tvBateria = findViewById(R.id.tvBateria);
+        tvcamFront = findViewById(R.id.tvCamFrontal);
+        tvcamPrincipal = findViewById(R.id.tvCamPrincipal);
+        tvCantParlantes = findViewById(R.id.tvCantidadParlantes);
+        tvCapacBateria = findViewById(R.id.tvCapacidadBat);
+        tvGpu = findViewById(R.id.tvGPU);
+        tvHMDI = findViewById(R.id.tvHMDI);
+        tvLinea = findViewById(R.id.tvlinea);
+        tvMarca = findViewById(R.id.tvMarca);
+        tvMarcaProcesador = findViewById(R.id.tvMarcaProcesador);
+        tvMemInterna = findViewById(R.id.tvMemInterna);
+        tvMemRam = findViewById(R.id.tvRAM);
+        tvModelProcesador = findViewById(R.id.tvModeloProcesador);
+        tvNuleos = findViewById(R.id.tvNucleos);
+        tvPeso = findViewById(R.id.tvPeso);
+        tvRed = findViewById(R.id.tvRed);
+        tvResolucion = findViewById(R.id.tvResolucion);
+        tvSo = findViewById(R.id.tvSO);
+        tvTamPantalla = findViewById(R.id.tvTamañoPantalla);
+        tvTipoPantalla = findViewById(R.id.tvTipoPantalla);
+        tvRed = findViewById(R.id.tvRed);
+        tvUsb = findViewById(R.id.tvUSB);
+        tvversionSO = findViewById(R.id.tvVSO);
+        tvWifi = findViewById(R.id.tvWIfi);
+        //
+
+        //Linears
+
+        linearBateria = findViewById(R.id.bateriaLinearLay);
+        linearcamaraFront = findViewById(R.id.camaraFrontLinear);
+        linearcamaraPrincipal = findViewById(R.id.camaraPrinciLinear);
+        linearCantidadParlantes = findViewById(R.id.cantParlantesLinear);
+        linearCapacBateria = findViewById(R.id.capacidadBateriaLinear);
+        linearGPU = findViewById(R.id.gpuLinear);
+        linearHMDI = findViewById(R.id.hmdiLinear);
+        linearMemInt = findViewById(R.id.memoriaInternaLinear);
+        linearMemRam = findViewById(R.id.memoriaRamLinear);
+        linearMarcaProcesador = findViewById(R.id.marcaProcesadorLinear);
+        linearModelProcesador = findViewById(R.id.modeloProcesadorLinear);
+        linearNucleos = findViewById(R.id.nucleosLinear);
+        linearRed = findViewById(R.id.redLinear);
+        linearSO = findViewById(R.id.soLinear);
+        linearUSB = findViewById(R.id.usbLinear);
+        linearVSO = findViewById(R.id.versionSOLinear);
+        linearWifi = findViewById(R.id.wifiLinear);
+        //
+
+        reference = FirebaseDatabase.getInstance().getReference("Productos");
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Productos productos = dataSnapshot.getValue(Productos.class);
 
-                assert productos != null;
-                if (productos.getGpu().equals(""))
-                {
-                    linearGpu.setVisibility(View.GONE);
-                    tvgName.setVisibility(View.GONE);
-                 gpu.setVisibility(View.GONE);
-                } else {
-                    linearGpu.setVisibility(View.VISIBLE);
-                    tvgName.setVisibility(View.VISIBLE);
-                    gpu.setVisibility(View.VISIBLE);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    Productos productos = snapshot.getValue(Productos.class);
+
+                    assert productos != null;
+                    Glide.with(getApplicationContext()).load(productos.getImagenProducto()).into(imagenProducto);
+
+                    /*
+                    tvAlto.setText(productos.getAltura());
+                    tvAncho.setText(productos.getAncho());
+                    tvLinea.setText(productos.getLinea());
+                    tvMarca.setText(productos.getMarca());
+                    tvPeso.setText(productos.getPeso());
+                    tvResolucion.setText(productos.getResolucion());
+                    tvTamPantalla.setText(productos.getTamañoPantalla());
+                    tvTipoPantalla.setText(productos.getTipoPantalla());
+                    tvUsb.setText(productos.getUsb());
+
+
+                    if (productos.getBateria().equals("") || productos.getCamaraFrontal().equals("") || productos.getCamaraPrincipal().equals("") ||
+                            productos.getCantidadParlantes().equals("") || productos.getCapacidadBateria().equals("") || productos.getGpu().equals("") ||
+                            productos.getHdmi().equals("") || productos.getMemoriaInterna().equals("") || productos.getMemoriaRam().equals("") ||
+                            productos.getMarcaProcesador().equals("") || productos.getModeloProcesador().equals("") || productos.getNucleos().equals("") ||
+                            productos.getRed().equals("") || productos.getSo().equals("") || productos.getVersionSO().equals("") || productos.getWifi().equals("")) {
+
+                        linearBateria.setVisibility(View.GONE);
+                        linearcamaraFront.setVisibility(View.GONE);
+                        linearcamaraPrincipal.setVisibility(View.GONE);
+                        linearCantidadParlantes.setVisibility(View.GONE);
+                        linearCapacBateria.setVisibility(View.GONE);
+                        linearGPU.setVisibility(View.GONE);
+                        linearHMDI.setVisibility(View.GONE);
+                        linearMemInt.setVisibility(View.GONE);
+                        linearMemRam.setVisibility(View.GONE);
+                        linearMarcaProcesador.setVisibility(View.GONE);
+                        linearModelProcesador.setVisibility(View.GONE);
+                        linearNucleos.setVisibility(View.GONE);
+                        linearRed.setVisibility(View.GONE);
+                        linearSO.setVisibility(View.GONE);
+                        linearVSO.setVisibility(View.GONE);
+                        linearWifi.setVisibility(View.GONE);
+
+                    } else {
+
+                        linearBateria.setVisibility(View.VISIBLE);
+                        linearcamaraFront.setVisibility(View.VISIBLE);
+                        linearcamaraPrincipal.setVisibility(View.VISIBLE);
+                        linearCantidadParlantes.setVisibility(View.VISIBLE);
+                        linearCapacBateria.setVisibility(View.VISIBLE);
+                        linearGPU.setVisibility(View.VISIBLE);
+                        linearHMDI.setVisibility(View.VISIBLE);
+                        linearMemInt.setVisibility(View.VISIBLE);
+                        linearMemRam.setVisibility(View.VISIBLE);
+                        linearMarcaProcesador.setVisibility(View.VISIBLE);
+                        linearModelProcesador.setVisibility(View.VISIBLE);
+                        linearNucleos.setVisibility(View.VISIBLE);
+                        linearRed.setVisibility(View.VISIBLE);
+                        linearSO.setVisibility(View.VISIBLE);
+                        linearVSO.setVisibility(View.VISIBLE);
+                        linearWifi.setVisibility(View.VISIBLE);
+
+                        tvBateria.setText(productos.getBateria());
+                        tvcamFront.setText(productos.getCamaraFrontal());
+                        tvcamPrincipal.setText(productos.getCamaraPrincipal());
+                        tvCantParlantes.setText(productos.getCantidadParlantes());
+                        tvCapacBateria.setText(productos.getCapacidadBateria());
+                        tvGpu.setText(productos.getGpu());
+                        tvHMDI.setText(productos.getHdmi());
+                        tvMemInterna.setText(productos.getMemoriaInterna());
+                        tvMemRam.setText(productos.getMemoriaRam());
+                        tvMarcaProcesador.setText(productos.getMarcaProcesador());
+                        tvModelProcesador.setText(productos.getModeloProcesador());
+                        tvNuleos.setText(productos.getNucleos());
+                        tvRed.setText(productos.getRed());
+                        tvSo.setText(productos.getSo());
+                        tvversionSO.setText(productos.getVersionSO());
+                        tvWifi.setText(productos.getWifi());
+                    }*/
                 }
-
-                Glide.with(getApplicationContext()).load(productos.getImagenProducto()).into(imagenProducto);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                System.out.println("Error: " + databaseError);
             }
         });
-
     }
 }
--- app/src/main/java/com/example/tucshop/Modelo/Productos.java
+++ app/src/main/java/com/example/tucshop/Modelo/Productos.java
@@ -5,16 +5,68 @@ public class Productos {
     private String imagenProducto;
     private String nombreProducto;
     private String montoProducto;
+    private String altura;
+    private String ancho;
+    private String bateria;
+    private String camaraFrontal;
+    private String camaraPrincipal;
+    private String cantidadParlantes;
+    private String capacidadBateria;
     private String gpu;
+    private String hdmi;
+    private String linea;
+    private String marca;
+    private String marcaProcesador;
+    private String memoriaRam;
+    private String memoriaInterna;
+    private String modeloProcesador;
+    private String nucleos;
+    private String peso;
+    private String red;
+    private String resolucion;
+    private String so;
+    private String tamañoPantalla;
+    private String tipoPantalla;
+    private String usb;
+    private String versionSO;
+    private String wifi;
 
     public Productos() {
     }
 
-    public Productos(String imagenProducto, String nombreProducto, String montoProducto, String gpu) {
+    public Productos(String imagenProducto, String nombreProducto, String montoProducto, String altura, String ancho, String bateria,
+                     String camaraFrontal, String camaraPrincipal, String cantidadParlantes, String capacidadBateria, String gpu, String hdmi,
+                     String linea, String marca, String marcaProcesador, String memoriaRam, String memoriaInterna, String modeloProcesador, String nucleos, String peso,
+                     String red, String resolucion, String so, String tamañoPantalla, String tipoPantalla, String usb, String versionSO, String wifi)
+    {
         this.imagenProducto = imagenProducto;
         this.nombreProducto = nombreProducto;
         this.montoProducto = montoProducto;
+        this.altura = altura;
+        this.ancho = ancho;
+        this.bateria = bateria;
+        this.camaraFrontal = camaraFrontal;
+        this.camaraPrincipal = camaraPrincipal;
+        this.cantidadParlantes = cantidadParlantes;
+        this.capacidadBateria = capacidadBateria;
         this.gpu = gpu;
+        this.hdmi = hdmi;
+        this.linea = linea;
+        this.marca = marca;
+        this.marcaProcesador = marcaProcesador;
+        this.memoriaRam = memoriaRam;
+        this.memoriaInterna = memoriaInterna;
+        this.modeloProcesador = modeloProcesador;
+        this.nucleos = nucleos;
+        this.peso = peso;
+        this.red = red;
+        this.resolucion = resolucion;
+        this.so = so;
+        this.tamañoPantalla = tamañoPantalla;
+        this.tipoPantalla = tipoPantalla;
+        this.usb = usb;
+        this.versionSO = versionSO;
+        this.wifi = wifi;
     }
 
     public String getImagenProducto() {
@@ -41,9 +93,203 @@ public class Productos {
         this.montoProducto = montoProducto;
     }
 
-    public String getGpu(){
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(String ancho) {
+        this.ancho = ancho;
+    }
+
+    public String getBateria() {
+        return bateria;
+    }
+
+    public void setBateria(String bateria) {
+        this.bateria = bateria;
+    }
+
+    public String getCamaraFrontal() {
+        return camaraFrontal;
+    }
+
+    public void setCamaraFrontal(String camaraFrontal) {
+        this.camaraFrontal = camaraFrontal;
+    }
+
+    public String getCamaraPrincipal() {
+        return camaraPrincipal;
+    }
+
+    public void setCamaraPrincipal(String camaraPrincipal) {
+        this.camaraPrincipal = camaraPrincipal;
+    }
+
+    public String getCantidadParlantes() {
+        return cantidadParlantes;
+    }
+
+    public void setCantidadParlantes(String cantidadParlantes) {
+        this.cantidadParlantes = cantidadParlantes;
+    }
+
+    public String getCapacidadBateria() {
+        return capacidadBateria;
+    }
+
+    public void setCapacidadBateria(String capacidadBateria) {
+        this.capacidadBateria = capacidadBateria;
+    }
+
+    public String getGpu() {
         return gpu;
     }
 
-    public void setGpu(String gpu){this.gpu = gpu;}
+    public void setGpu(String gpu) {
+        this.gpu = gpu;
+    }
+
+    public String getHdmi() {
+        return hdmi;
+    }
+
+    public void setHdmi(String hdmi) {
+        this.hdmi = hdmi;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarcaProcesador(){
+        return marcaProcesador;
+    }
+
+    public void setMarcaProcesador(String marcaProcesador){
+        this.marcaProcesador = marcaProcesador;
+    }
+
+    public String getMemoriaRam() {
+        return memoriaRam;
+    }
+
+    public void setMemoriaRam(String memoriaRam) {
+        this.memoriaRam = memoriaRam;
+    }
+
+    public String getMemoriaInterna() {
+        return memoriaInterna;
+    }
+
+    public void setMemoriaInterna(String memoriaInterna) {
+        this.memoriaInterna = memoriaInterna;
+    }
+
+    public String getModeloProcesador() {
+        return modeloProcesador;
+    }
+
+    public void setModeloProcesador(String modeloProcesador) {
+        this.modeloProcesador = modeloProcesador;
+    }
+
+    public String getNucleos() {
+        return nucleos;
+    }
+
+    public void setNucleos(String nucleos) {
+        this.nucleos = nucleos;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getRed() {
+        return red;
+    }
+
+    public void setRed(String red) {
+        this.red = red;
+    }
+
+    public String getResolucion() {
+        return resolucion;
+    }
+
+    public void setResolucion(String resolucion) {
+        this.resolucion = resolucion;
+    }
+
+    public String getSo() {
+        return so;
+    }
+
+    public void setSo(String so) {
+        this.so = so;
+    }
+
+    public String getTamañoPantalla() {
+        return tamañoPantalla;
+    }
+
+    public void setTamañoPantalla(String tamañoPantalla) {
+        this.tamañoPantalla = tamañoPantalla;
+    }
+
+    public String getTipoPantalla() {
+        return tipoPantalla;
+    }
+
+    public void setTipoPantalla(String tipoPantalla) {
+        this.tipoPantalla = tipoPantalla;
+    }
+
+    public String getUsb() {
+        return usb;
+    }
+
+    public void setUsb(String usb) {
+        this.usb = usb;
+    }
+
+    public String getVersionSO() {
+        return versionSO;
+    }
+
+    public void setVersionSO(String versionSO) {
+        this.versionSO = versionSO;
+    }
+
+    public String getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(String wifi) {
+        this.wifi = wifi;
+    }
 }
